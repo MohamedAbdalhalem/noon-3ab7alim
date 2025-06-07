db781d13222bdc83e7eca37e055970efa30e186f
MohamedAbdalhalem <mohamed3ab7alem@gmail.com>
2025-06-07 18:38:08 +0300

    handle the error

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import  { useContext } from 'react'
 import { AuthenticationContext } from '../../context/AuthenticationContext'
 import axios from 'axios'
 import { useQuery } from '@tanstack/react-query'
