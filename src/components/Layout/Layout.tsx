612c8c870823929a1a535574caefc0d6bece0568
MohamedAbdalhalem <mohamed3ab7alem@gmail.com>
2025-07-07 09:45:39 +0300

    update navbar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import Navbar from './../Navbar/Navbar';
+import { Nav } from './../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 
 export default function Layout() {
   return (
       <>
-        <Navbar />
+        <Nav />
       <Outlet />
       <Footer/>
       </>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,123 +1,105 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarToggle,
+  Dropdown,
+  DropdownItem,
+  DropdownDivider,
+} from "flowbite-react";
 import { useContext, useEffect, useState } from "react";
-import {  Link, NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { AuthenticationContext } from "../../context/AuthenticationContext";
 import { CartContext } from "../../context/CartContext";
 
-export default function Navbar() {
-  const [isDrak, setIsDark] = useState('Light-mode')
-  const {numOfCartItems} = useContext(CartContext)
-  function handleDarkLightMode() {
-    
-    if (localStorage.getItem('mode') === 'Dark-mode') {
-      localStorage.setItem('mode','Light-mode')
-      document.documentElement.classList.remove('dark')
-      setIsDark('Light-mode')
-    } else {
-      localStorage.setItem('mode','Dark-mode')
-      document.documentElement.classList.add('dark')
-      setIsDark('Dark-mode')
-    }
-  }
-  useEffect(() => {
-    if (localStorage.getItem('mode') === 'Dark-mode') {
-      document.documentElement.classList.add('dark')
-      setIsDark('Dark-mode')
-    } else {
-      document.documentElement.classList.remove('dark')
-      setIsDark('Light-mode')
-    }
-  },[])
-  const { token, setToken } = useContext(AuthenticationContext)
-  const navigatToSignIn = useNavigate()
-  function handleLogout() {
-    setToken(null)
-    localStorage.removeItem('token')
-    navigatToSignIn ('/sign-in')
-  }
-  return (
-    <div>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900 shadow-lg">
-  <div className="flex flex-wrap items-center justify-between mx-auto p-4">
-    <Link  to='' className="flex items-center space-x-3 rtl:space-x-reverse">
-      <span className="font-bold text-blue-700 text-2xl">Noon 3ab7alim</span>
-    </Link>
-    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-      <span className="sr-only">Open main menu</span>
-      <svg className="w-5 h-5 cursor-pointer" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 1h15M1 7h15M1 13h15" />
-      </svg>
-    </button>
-    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul className="font-medium flex flex-col md:items-center p-4 md:p-0 mt-4 border border-gray-200 rounded-lg bg-gray-50 md:flex-row md:space-x-5 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        {token && <>
-                <li>
-          <NavLink to='' className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to='/categories' className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Categories</NavLink>
-        </li>
-        <li>
-          <NavLink to='/brands' className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Brands</NavLink>
-                </li>
-                <li>
-          <NavLink to='/wishlist' className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">WishList</NavLink>
-              </li>
-              <li>
-          <NavLink to='/allorders' className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">orders</NavLink>
-              </li>
-        <li>
-                
-                  <div className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                    <button type="button" className="relative inline-flex items-center  text-sm font-medium text-center text-white  rounded-lg ">
-                      <NavLink to='/cart' className="fa-solid fa-cart-shopping text-gray-900 text-lg dark:text-white"></NavLink>
-                      {numOfCartItems ? <div className="absolute  inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-blue-500 border-2 border-white rounded-full -top-4 -end-4 dark:border-gray-900">{ numOfCartItems }</div>:''}
-                    </button>
-                  </div>
-        </li>
-              </>}      
-        
-        
-        
-{/* drop down meun */}
- <li>            
-<div>
-  <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className=" cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  px-3 py-1.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Settings<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m1 1 4 4 4-4" />
-    </svg>
-  </button>
-  {/* Dropdown menu */}
-  <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44  dark:bg-gray-700">
-      <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-      {token ? <><li>
-        <NavLink to='/profile' className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Profile</NavLink>
-                      </li>
-                        <li>
-        <span onClick={handleLogout} className="block cursor-pointer px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Log-out</span>
-      </li>
-                      </> : <>
-                          <li>
-        <NavLink to='/sign-in' className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign-in</NavLink>
-      </li>
-      <li>
-        <NavLink to='/sign-up' className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign-up</NavLink>
-      </li>
-                      </>
-      }
-      <li>
-                        <span onClick={handleDarkLightMode} className="block cursor-pointer px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{ isDrak }</span>
-      </li>
-      
-      
-    </ul>
-  </div>
-</div>
+export function Nav() {
+  const [isDrak, setIsDark] = useState("Light-mode");
+  const { token, setToken } = useContext(AuthenticationContext);
+  const { numOfCartItems } = useContext(CartContext);
+  const navigate = useNavigate();
 
-    
-          </li>
-      </ul>
-    </div>
-  </div>
-      </nav>
-    </div>
-  )
+  const handleLogout = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+    navigate("/sign-in");
+  };
+
+  const handleDarkLightMode = () => {
+    if (localStorage.getItem("mode") === "Dark-mode") {
+      localStorage.setItem("mode", "Light-mode");
+      document.documentElement.classList.remove("dark");
+      setIsDark("Light-mode");
+    } else {
+      localStorage.setItem("mode", "Dark-mode");
+      document.documentElement.classList.add("dark");
+      setIsDark("Dark-mode");
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("mode") === "Dark-mode") {
+      document.documentElement.classList.add("dark");
+      setIsDark("Dark-mode");
+    } else {
+      document.documentElement.classList.remove("dark");
+      setIsDark("Light-mode");
+    }
+  }, []);
+
+  return (
+    <Navbar className="rounded-none shadow-md dark:bg-gray-900" fluid>
+      <Link to="/">
+        <NavbarBrand>
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white text-blue-700">
+            Noon 3ab7alim
+          </span>
+        </NavbarBrand>
+      </Link>
+
+      <div className="flex md:order-2  items-center gap-2">
+        <Dropdown label={<span className="text-gray-900 cursor-pointer dark:text-white">Settings</span>} inline>
+          {token ? (
+            <>
+              <DropdownItem  as={NavLink} to="/profile">Profile</DropdownItem>
+              <DropdownItem onClick={handleLogout}>Log-out</DropdownItem>
+            </>
+          ) : (
+            <>
+              <DropdownItem as={NavLink} to="/sign-in">Sign-in</DropdownItem>
+              <DropdownItem as={NavLink} to="/sign-up">Sign-up</DropdownItem>
+            </>
+          )}
+          <DropdownDivider />
+          <DropdownItem onClick={handleDarkLightMode}>{isDrak}</DropdownItem>
+        </Dropdown>
+
+        {/* Cart */}
+        {token && (
+          <p className="relative mr-2">
+            <NavLink to='/cart' className="fa-solid fa-cart-shopping text-lg text-gray-900 dark:text-white">
+            </NavLink>
+            {numOfCartItems > 0 && (
+              <span className="absolute -top-2 -right-2 bg-blue-600 text-white text-xs px-1 rounded-full">
+                {numOfCartItems}
+              </span>
+            )}
+          </p>
+        )}
+
+        
+      </div>
+      {token && <>
+        <NavbarToggle />
+        <NavbarCollapse>
+          <NavLink to='/' className='text-gray-900 mb-2 md:mb-0  dark:text-white font-bold  '>Home</NavLink>
+          <NavLink to='/categories' className='text-gray-900 mb-2 md:mb-0  dark:text-white font-bold'>Categories</NavLink>
+          <NavLink to='/brands' className='text-gray-900 mb-2 md:mb-0  dark:text-white font-bold'>Brands</NavLink>
+          <NavLink to='/wishlist' className='text-gray-900 mb-2 md:mb-0  dark:text-white font-bold'>Wishlist</NavLink>
+          <NavLink to='/allorders' className='text-gray-900 mb-2 md:mb-0  dark:text-white font-bold'>Allorders</NavLink>
+      </NavbarCollapse>
+      </>
+        }
+      
+    </Navbar>
+  );
 }
